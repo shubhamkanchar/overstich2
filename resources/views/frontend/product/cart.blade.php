89eb170ed75ab7ecbba43feb06edf45dbc7480d9
Abubuckersidhiq <=cse18.abubucker@petengg.ac.in>
2023-12-26 08:13:29 +0530

    Fixed Cart issue

--- resources/views/frontend/product/cart.blade.php
+++ resources/views/frontend/product/cart.blade.php
@@ -16,7 +16,113 @@
 @section('content')
     <div class="container">
         <div class="row mt-5 mb-5 justify-content-end row-gap-5 gap-2 cart-contents">
-            {!! $cartContentView !!}
+            <div class="col-12 col-md-8 bag-items">
+                @if (count($cartItems) == 0)
+                    <h1 class="fw-bold ms-md-4 text-center text-md-start"> Bag</h1>
+                    <p class="ms-md-5 fs-md-3 text-center text-md-start">There Are No items in Bag</p>
+                @else
+                    <h1 class="fw-bold ms-md-4 text-center text-md-start"> Bag - {{ count($cartItems) }} items</h1>
+                    <div class="card mt-4 bg-none border-0">
+                        @foreach ($cartItems as $key => $item)
+                            @php $product = $products[$item->id]; @endphp
+                            @php $productSize = $product->sizes->where('size', $item->options?->size)->first() @endphp
+                            
+                            <div class="card-body cart-items shadow-lg m-2 rounded-lg border border-2 @if ($productSize->quantity <= 0) bg-gray @else bg-light @endif">
+                                <div class="row">
+                                    <div class="col-lg-3 col-md-12 mb-4 mb-lg-0">
+                                        <!-- Image -->
+                                        <div class="bg-image hover-overlay hover-zoom ripple rounded"
+                                            data-mdb-ripple-color="light">
+                                            <img src="{{ asset($item?->options?->image) }}"
+                                                class="w-100" alt="Blue Jeans Jacket" style="height: 150px"/>
+                                            <a href="#!">
+                                                <div class="mask" style="background-color: rgba(251, 251, 251, 0.2)"></div>
+                                            </a>
+                                        </div>
+                                        <!-- Image -->
+                                    </div>
+            
+                                    <div class="col-lg-5 col-md-6 mb-4 mb-lg-0">
+                                        <!-- Data -->
+                                        <p><strong>{{ $item->name }}</strong></p>
+                                        @if (!empty($product->color))
+                                            <p>Color: {{ $product->color}}</p>
+                                        @endif
+                                        <p>Size: {{ $item->options?->size }}</p>
+                                        <form action="{{ route('cart.update') }}" class="cart-remove-form d-inline" method="POST">
+                                            @csrf
+                                            <input type="hidden" value="{{ $key }}" name="id">
+                                            <input type="hidden" name="quantity" value="0"/>
+                                            <button type="submit" class="border-0 fs-3" data-mdb-toggle="tooltip" title="Remove item"> 
+                                                <i class="bi bi-trash text-danger"></i>
+                                            </button>
+                                        </form>
+                                        
+                                        <i class="bi  @if(in_array($product->id, $productIds)) bi-heart-fill text-danger @else bi-heart  @endif  fs-3 me-4 add-to-wishlist" data-add-route="{{ route('wishlist.add-wishlist', $product->id) }}" data-remove-route="{{ route('wishlist.remove-wishlist', $product->id) }}"></i>
+                                    </div>
+            
+                                    <div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
+                                        <form class="cart-update-form d-flex my-4" action="{{ route('cart.update') }}" method="POST">
+                                            @csrf
+                                            <input type="hidden" name="id" value="{{ $key}}">
+                                            <button type="submit" class="border-0 fs-3 text-center mb-2 me-1" style="width: 40px; height: 40px; border-radius: 50%"
+                                                onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
+                                                <i class="bi bi-dash"></i>
+                                            </button>
+            
+                                            <div class="form-outline">
+                                                <input type="number" name="quantity" min="1" max="{{ $productSize->quantity}}" value="{{ $item->qty }}" type="number" class="form-control text-center" style="width: 50px; height: 40px;" />
+                                            </div>
+            
+                                            <button type="submit" class="border-0 fs-3 text-center mb-2 me-1" style="width: 40px; height: 40px; border-radius: 50%"
+                                                onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
+                                                <i class="bi bi-plus"></i>
+                                            </button>
+                                        </form>
+                                        <p class="text-start text-md-center">
+                                            <strong> ₹<span id="itemPrice-{{$item->id}}"> {{ number_format(($item->price * $item->qty), 2) }} </span></strong>
+                                        </p>
+                                        @if ($productSize->quantity <= 0)
+                                            <span class="ps-4 fs-5 mt-3 d-block">Product Not Available</span>
+                                        @elseif ($item->qty > $productSize->quantity)
+                                            <span class="ps-4 fs-5 mt-3 d-block">Only {{ $productSize->quantity }} quantity is left</span>
+                                        @endif
+            
+                                        @if ($productSize->quantity <= 10 and !($item->qty > $productSize->quantity))
+                                            <span class="ps-4 text-danger fs-5 mt-3 d-block text-nowrap">!Hurry Only {{ $productSize->quantity }} quantity is left</span>
+                                        @endif
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                @endif
+            
+            </div>
+            <div class="col-12 col-md-3 px-4">
+                <h2 class="fw-bold">Summary</h2>
+                <div class="d-flex justify-content-between">
+                    <span>Subtotal</span>
+                    <span id="subTotal">{{ count($cartItems) > 0 ? $totalOriginalPrice : '-' }}</span>
+                </div>
+                <div class="d-flex justify-content-between">
+                    <span>Delivery Charges</span>
+                    <span id="deliveryCharges">{{ count($cartItems) > 0 ? $deliveryCharges : '-' }}</span>
+                </div>
+                <div class="d-flex justify-content-between">
+                    <span>Discount</span>
+                    <span id="totalDiscount">{{ count($cartItems) > 0 ? $totalDiscount : '-' }}</span>
+                </div>
+                <hr>
+                <div class="d-flex justify-content-between">
+                    <span>Total</span>
+                    <span id="totalAmount"> {{ count($cartItems) > 0 ? $totalOriginalPrice - $totalDiscount + $deliveryCharges : '-' }}</span>
+                </div>
+                <hr>
+                <form action="{{ route('checkout') }}" class="d-inline" method="get">
+                    <input type="submit" value="Place Order" class="bg-dark text-white px-3 py-1 mt-md-4 rounded-5">
+                </form>
+            </div>
         </div>
     </div>
 @endsection
@@ -39,6 +145,7 @@
             $(document).on('submit','.cart-remove-form', function(event) {
                 event.preventDefault();
                 let data = $(this).serialize();
+                let cardBody = $(this).closest('.card-body');
                 Swal.fire({
                     title: 'Are you sure?',
                     text: 'This item will removed from the cart',
@@ -51,11 +158,12 @@
                 }).then((result) => {
                     
                     if (result.isConfirmed) { 
-                        $('#popup-overlay').removeClass('d-none')
-                        $('.spinner').removeClass('d-none')
                         updateQuantity(data);
-                        $('#popup-overlay').addClass('d-none')
-                        $('.spinner').addClass('d-none')
+                        cardBody.remove();
+                        if($('.cart-items').length == 0) {
+                            $('.bag-items').html('<h1 class="fw-bold ms-md-4 text-center text-md-start"> Bag</h1>' +
+                            '<p class="ms-md-5 fs-md-3 text-center text-md-start">There Are No items in Bag</p>');
+                        }
                     }
                     
                 });
@@ -73,9 +181,15 @@
                     $('.spinner').removeClass('d-none')
                 },
                 success: (res) =>{
-                    $('.cart-contents').html(res.cartContentView)
+                    let item = res.updatedItem;
+                    $('#totalDiscount').text(res.totalDiscount == 0 ? '-' : res.totalDiscount.toFixed(2));
+                    $('#deliveryCharges').text(res.deliveryCharges == 0 ? '-' : res.deliveryCharges.toFixed(2));
+                    $('#totalAmount').text((res.totalOriginalPrice - res.totalDiscount + res.deliveryCharges) == 0 ? '-' : (res.totalOriginalPrice - res.totalDiscount + res.deliveryCharges).toFixed(2));
+                    $('#subTotal').text(res.totalOriginalPrice == 0 ? '-' : res.totalOriginalPrice.toFixed(2));
+                    $('#itemPrice-'+ item.id).text((item.price * item.qty).toFixed(2));
                 },
                 error: (err) => {
+                    
                 },
                 beforeSend: () => {
                     $('#popup-overlay').addClass('d-none')
