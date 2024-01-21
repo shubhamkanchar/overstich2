329f1d19f8e9a597c657bd6e059db96f64449b6d
shubhamkanchar <shubhamkanchar687@gmail.com>
2024-01-22 01:21:44 +0530

    index

--- resources/views/backend/seller/order/index.blade.php
+++ resources/views/backend/seller/order/index.blade.php
@@ -5,7 +5,7 @@
         <div class="card">
             <div class="card-header">
                 Manage Orders
-                @if (empty($user->activeWarehouse))
+                @if (empty(auth()->user()->activeWarehouse))
                     <div class="alert alert-warning alert-dismissible fade show" role="alert">
                         Create warehouse and set it default to proceed with orders
                         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
